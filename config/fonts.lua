cde7dccc03d4660a97c00b64117b98c5dc0c967d
mrsauravsahu <mrsauravsahu@outlook.com>
2026-02-26 23:16:36 +0530

    chore: use custom font for mac

--- config/fonts.lua
+++ config/fonts.lua
@@ -5,7 +5,13 @@ local font_size = platform.is_mac and 18 or 16
 
 return {
   font_size = font_size,
-  font=wezterm.font_with_fallback {
+  font= platform.is_mac and
+    wezterm.font_with_fallback {
+      {
+        family = "Spot Mono",
+      }
+    }
+    or wezterm.font_with_fallback {
     {
       family = "UbuntuMono Nerd Font Mono",
     },
@@ -19,7 +25,9 @@ return {
       family = 'JetBrains Mono',
       weight = 'Medium',
     },
-    { family = 'Terminus', weight = 'Bold' },
+    {
+      family = 'Terminus', weight = 'Bold'
+    },
     'Noto Color Emoji',
   }
   --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
